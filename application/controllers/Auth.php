e8e2dafee52bc6b12fc3b6fdfdb9ec5c7a9752ba
wdamayo <winnie@treon.com>
2017-11-08 10:53:09 +0800

    autoload ion auth helpers and libraries

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,9 +5,6 @@ class Auth extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->database();
-		$this->load->library(array('ion_auth','form_validation'));
-		$this->load->helper(array('url','language'));
 
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 
