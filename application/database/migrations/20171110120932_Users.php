9c6c87d05cabf6f61f962876578daa3e47ce7702
wdamayo <winnie131212592@gmail.com>
2017-11-10 15:27:06 +0800

    add users migration

---
+++ application/database/migrations/20171110120932_Users.php
@@ -0,0 +1,58 @@
+<?php
+
+class Migration_Users extends CI_Migration {
+
+    public function up() {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => TRUE
+            ),
+            'first_name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ),
+            'middle_name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+                'null' => true
+            ),
+            'last_name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+                'null' => true
+            ),
+            'birth_date' => array(
+                'type' => 'DATE'
+            ),
+            'username' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ),
+            'password' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ),
+            'group_id' => array(
+                'type' => 'INT',
+                'constraint' => 11
+            ),
+            'created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP',
+            'updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+            'active' => array(
+                'type' => 'TINYINT',
+                'null' => false,
+                'default' => 1
+            )
+
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('users');
+    }
+
+    public function down() {
+        $this->dbforge->drop_table('users');
+    }
+
+}
