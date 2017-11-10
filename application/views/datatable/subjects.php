3ddf23cf3701df5298e2254f388ed8998f513019
wdamayo <winnie@treon.com>
2017-11-10 10:21:15 +0800

    remove trash and refactor script with separate folder

---
+++ application/views/datatable/subjects.php
@@ -0,0 +1,31 @@
+<script type="text/javascript">
+	
+$(function() {
+	//datatables
+var table = $('#subjects-table').DataTable({ 
+ 
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        "order": [], //Initial no order.
+ 
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo base_url('subjectsList')?>",
+            "type": "POST",
+            "data": {
+                '<?php csrfName(); ?>' : '<?php csrfHash(); ?>'
+            }
+        },
+ 
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        { 
+            "targets": [ 0 ], //first column / numbering column
+            "orderable": false, //set not orderable
+        },
+        ],
+ 
+    });
+});
+
+</script>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -11,66 +11,19 @@
 
 <script src="public/js/jquery-2.2.3.min.js"></script>
 <script src="public/js/bootstrap.min.js"></script>
-
 <!-- Data Tables -->
 <script type="text/javascript" src="<?= base_url('public/js/jquery.dataTables.min.js'); ?>"></script>
 
+
+
+
+<?php $this->load->view('datatable/users'); ?>
+<?php $this->load->view('datatable/subjects'); ?>
+
+
+
+
 <script type="text/javascript">
-$(document).ready(function() {
-    //datatables
-    $('#users-table').DataTable({ 
- 
-        "processing": true, //Feature control the processing indicator.
-        "serverSide": true, //Feature control DataTables' server-side processing mode.
-        "order": [], //Initial no order.
-        // Load data for the table's content from an Ajax source
-        "ajax": {
-            "url": "<?php echo base_url('usersList')?>",
-            "type": "POST",
-            "data": {
-                '<?php csrfName(); ?>' : '<?php csrfHash(); ?>'
-            }
-        },
-        //Set column definition initialisation properties.
-        "columnDefs": [
-        { 
-            "targets": [ 0 ], //first column / numbering column
-            "orderable": false, //set not orderable
-        },
-        ],
- 
-    });
-
-    //datatables
-var table = $('#subjects-table').DataTable({ 
- 
-        "processing": true, //Feature control the processing indicator.
-        "serverSide": true, //Feature control DataTables' server-side processing mode.
-        "order": [], //Initial no order.
- 
-        // Load data for the table's content from an Ajax source
-        "ajax": {
-            "url": "<?php echo base_url('subjectsList')?>",
-            "type": "POST",
-            "data": {
-                '<?php csrfName(); ?>' : '<?php csrfHash(); ?>'
-            }
-        },
- 
-        //Set column definition initialisation properties.
-        "columnDefs": [
-        { 
-            "targets": [ 0 ], //first column / numbering column
-            "orderable": false, //set not orderable
-        },
-        ],
- 
-    });
-
- 
-});
-
-
 $(function() {
     <?php if($this->session->userdata('modalChangePassword')): ?>
         $('#modal-change-password').modal();
@@ -82,7 +35,6 @@ $(function() {
         $('#modal-info').modal();
     <?php endif; ?>
 });
-
 </script>
 
 
