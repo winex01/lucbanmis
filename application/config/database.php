83cdbab5390756602900de8d1cc6b7e8a8593ad7
winnie damayo <winnie131212592@gmail.com>
2017-11-12 17:04:33 +0800

    temporary database.php config

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '45.77.240.212',
 	'username' => 'root',
 	// 'password' => '',
 	'password' => 'pass123',
