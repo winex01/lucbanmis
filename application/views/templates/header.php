acdee87cd6338f5d6c0546b0a9140bef766f7717
winnie <wdamayo@treon-inc.com>
2017-11-13 11:35:24 +0800

    add base url in scripts

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-    <link rel="stylesheet" href="public/css/bootstrap.css" media="screen">
-    <link href="public/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= base_url('public/css/bootstrap.css'); ?>" media="screen">
+    <link href="<?= base_url('public/css/jquery.dataTables.min.css'); ?>" rel="stylesheet">
 
  
 
