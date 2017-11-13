c2051f8bd98b092d5a3a340f6ddfdda96041ee2f
winnie damayo <winnie131212592@gmail.com>
2017-11-13 21:27:50 +0800

    testing cron tab

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -27,12 +27,7 @@
       <h3 class="panel-title">Login</h3>
     </div>
     <div class="panel-body">
-      test Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      testing
 
         <?php $this->load->view('flash/validationErrors'); ?>
 
