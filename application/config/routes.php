65f1d460c7f13f21dcf4bb98da633506434a60d2
winnie damayo <winnie131212592@gmail.com>
2017-11-15 00:47:29 +0800

    move up schooyear rotues

--- application/config/routes.php
+++ application/config/routes.php
@@ -6,6 +6,12 @@ $route['default_controller']   = 'auth/login';
 $route['404_override']         = '';
 $route['translate_uri_dashes'] = FALSE;
 
+// schoolyear
+$route['schoolyears']      = 'schoolyear/schoolyears';
+$route['schoolyearsList']  = 'schoolyear/ajax_list';
+$route['deleteSchoolYear'] = 'schoolyear/delete';
+
+
 //auth
 $route['login']  = 'auth/login';
 $route['logout'] = 'auth/logout';
@@ -39,18 +45,12 @@ $route['addSubjectPage']  = 'subject/addSubjectPage';
 $route['subjects']        = 'subject/subjects';
 $route['subjectsList']    = 'subject/ajax_list';
 
-// schoolyear
-$route['schoolyears']      = 'schoolyear/schoolyears';
-$route['schoolyearsList']  = 'schoolyear/ajax_list';
-$route['deleteSchoolYear'] = 'schoolyear/delete';
-
-$route['test']      = 'schoolyear/tests';
-
 // reports
 $route['printForm137'] = 'report/printForm137';
 
 // cron
 $route['gitpull'] = 'cron/gitpull';
 
+$route['test']      = 'schoolyear/tests';
 
 
