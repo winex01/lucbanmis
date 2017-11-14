77e2eaa3cf94612a60ac1d0634233d1c362a26da
winnie damayo <winnie131212592@gmail.com>
2017-11-15 01:39:15 +0800

    make gitpull route testing

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,4 @@ $route['schoolyearsList']  = 'schoolyear/ajax_list';
 $route['deleteSchoolYear'] = 'schoolyear/delete';
 
 
-$route['gitpull'] = 'cron/gitpull';
+$route['gitpull'] = 'schoolyear/schoolyears';
